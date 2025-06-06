1d89da15aa8300678ce03fe9282ea882f87d2ae3
Thanattsitt S. <166811468+Pigletpeakkung@users.noreply.github.com>
2025-06-06 19:30:23 +0700

    Create index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,15 @@
+// Animations
+export { default as ParticleBackground } from './animations/ParticleBackground';
+export { default as MoonAnimation } from './animations/MoonAnimation';
+export { default as TypeWriter } from './animations/TypeWriter';
+
+// Cards
+export { default as SkillCard } from './cards/SkillCard';
+export { default as ProjectCard } from './cards/ProjectCard';
+export { default as TestimonialCard } from './cards/TestimonialCard';
+
+// Gallery
+export { default as GalleryRow } from './gallery/GalleryRow';
+
+// UI
+export { default as Modal } from './ui/Modal';
